42001816188d4097d4a8df843ca6c0b2505acb98
Oli Gill <oliver.gill.89@gmail.com>
2022-01-16 13:20:45 +0200

    Update bookmarks.js

--- bookmarks.js
+++ bookmarks.js
@@ -4,44 +4,33 @@ const bookmarks = [
     title: "Daily",
     links: [
       { name: "Inbox", url: "https://inbox.google.com" },
-      { name: "GitHub", url: "https://github.com" },
-      { name: "Drive", url: "https://drive.google.com" },
+      { name: "Reuters", url: "https://www.reuters.com/" },
     ],
   },
   {
     title: "Media",
     links: [
       { name: "Youtube", url: "https://youtube.com" },
-      { name: "Netflix", url: "https://netflix.com" },
+      { name: "Sips", url: "https://twitch.tv/sips_" },
       { name: "Crunchyroll", url: "https://crunchyroll.com" },
-      {
-        name: "Amazon Prime",
-        url: "https://www.amazon.com/Amazon-Video",
-      },
+      { name: "Amazon Prime", url: "https://www.amazon.com/Amazon-Video" },
     ],
   },
   {
     title: "Reddit",
     links: [
-      { name: "/r/overwatch", url: "https://reddit.com/r/overwatch" },
+      { name: "/r/iracing", url: "https://reddit.com/r/iRacing" },
       {
-        name: "/r/pcmasterrace",
-        url: "https://reddit.com/r/pcmasterrace",
+        name: "/r/formula1",
+        url: "https://reddit.com/r/formula1",
       },
       { name: "/r/me_irl", url: "https://reddit.com/r/me_irl" },
-      {
-        name: "/r/battlestations",
-        url: "https://reddit.com/r/battlestations",
-      },
-      { name: "/r/unixporn", url: "https://reddit.com/r/unixporn" },
-      { name: "/r/news", url: "https://reddit.com/r/news" },
     ],
   },
   {
     title: "Social",
     links: [
-      { name: "Twitter", url: "https://twitter.com" },
-      { name: "Facebook", url: "https://facebook.com" },
+      { name: "Twitter", url: "https://twitter.com" }
     ],
   },
 ];
